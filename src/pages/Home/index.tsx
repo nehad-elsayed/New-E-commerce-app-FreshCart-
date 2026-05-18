b87fc2a6712f3de63e033a9bb2538d2aba401f07
Nehad <nehad.elsayed@robustastudio.com>
2026-05-18 22:24:42 +0300

    update ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import Products from "../Products";
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 mt-1">
-      <h2 className="font-bold text-primary-dark text-lg md:text-5xl">FreshCArt</h2>
+      <h2 className="font-bold text-center text-primary-dark text-lg md:text-5xl">FreshCArt</h2>
       <CategoriesSlider />
       <Products />
     </div>
