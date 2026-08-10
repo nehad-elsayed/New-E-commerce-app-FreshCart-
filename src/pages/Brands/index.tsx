4dce64ea16380ae9924faf6d333bd9f96a45534a
Nehad <nehad.elsayed@robustastudio.com>
2026-08-10 03:50:30 +0300

    refactor(brand): add a unique key prop for each brand

--- src/pages/Brands/index.tsx
+++ src/pages/Brands/index.tsx
@@ -11,10 +11,8 @@ export default function Brands() {
       <div className="container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 py-3 px-5">
         {data.map((brand: Brand) => {
           return (
-            // <div className="shadow-lg shadow-primary-dark/10 ">
-            //   <img className="text-center mx-auto rounded-lg hover:scale-50 transition-all duration-100 cursor-pointer transform " src={brand.image} alt={brand.name}/>
-            // </div>
-            <div className="shadow-lg shadow-primary-dark/10 group overflow-hidden rounded-lg">
+            
+            <div key={brand._id} className="shadow-lg shadow-primary-dark/10 group overflow-hidden rounded-lg">
               <img
                 className="mx-auto rounded-lg transition-transform duration-300 group-hover:scale-125 cursor-pointer"
                 src={brand.image}
